d78b22e0925a03c7e12c8526915aacacf7950dd7
loggerboy <loggerboy9325@gmail.com>
2024-06-03 15:15:35 -0400

    refactoring some code

--- lambda/main.go
+++ lambda/main.go
@@ -1,27 +1,59 @@
-package main
+package lambda
 
 import (
-	"context"
-
-	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/iam"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/lambda"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-// Define the request and response types
-type MyEvent struct {
-	Name string `json:"name"`
-}
-
-type MyResponse struct {
-	Message string `json:"message"`
-}
-
-func handleRequest(ctx context.Context, event MyEvent) (MyResponse, error) {
-	response := MyResponse{
-		Message: "Hello, World!",
+func CreateLambda(ctx *pulumi.Context) error {
+	role, err := iam.NewRole(ctx, "lambdaRole", &iam.RoleArgs{
+		AssumeRolePolicy: pulumi.String(`
+		{
+		 "Version": "2012-10-17",
+		 "Statement": [
+		   {
+		     "Action": "sts:AssumeRole",
+		     "Principal": {
+		       "Service": "lambda.amazonaws.com"
+		     },
+		     "Effect": "Allow",
+		     "Sid": ""
+		   }
+		 ]
+		}
+		`),
+	})
+	if err != nil {
+		return err
 	}
-	return response, nil
-}
 
-func main() {
-	lambda.Start(handleRequest)
+	// Attach necessary policies to the role
+	_, err = iam.NewRolePolicyAttachment(ctx, "lambdaRoleAttachment", &iam.RolePolicyAttachmentArgs{
+		Role:      role.Name,
+		PolicyArn: pulumi.String("arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"),
+	})
+	if err != nil {
+		return err
+	}
+
+	function, err := lambda.NewFunction(ctx, "myLambdaFunction", &lambda.FunctionArgs{
+		Code:        pulumi.NewFileArchive("lambda/function.zip"),
+		Runtime:     pulumi.String("provided.al2023"),
+		Role:        role.Arn,
+		Handler:     pulumi.String("main"),
+		Description: pulumi.String("My Lambda function"),
+		MemorySize:  pulumi.Int(128),
+		Timeout:     pulumi.Int(5),
+		Environment: &lambda.FunctionEnvironmentArgs{
+			Variables: pulumi.StringMap{
+				"ENV_VAR": pulumi.String("value"),
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+	ctx.Export("functionname", function.Name)
+	return nil
 }
---
+++ s3/s3.go
@@ -0,0 +1,29 @@
+package s3
+
+import (
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/s3"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func CreateS3Bucket(ctx *pulumi.Context) error {
+	bucket, err := s3.NewBucket(ctx, "testpulumibucket", nil)
+	if err != nil {
+		return err
+	}
+
+	files := []string{"index.html", "script.js", "images/CCP.png", "images/portrait.jpg",
+		"images/SAA.png", "images/SAP.png", "ResumeFinal.pdf"}
+
+	for _, file := range files {
+		_, err := s3.NewBucketObject(ctx, file, &s3.BucketObjectArgs{
+			Bucket:      bucket.ID(),
+			Source:      pulumi.NewFileAsset(file),
+			ContentType: pulumi.String("text/html"),
+		})
+		if err != nil {
+			return err
+		}
+		ctx.Export("bucket", bucket.ID())
+	}
+	return nil
+}
