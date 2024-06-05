f0d24cd3384cef6fc252fb446cf6288640c03cbc
loggerboy <loggerboy9325@gmail.com>
2024-06-05 11:08:59 -0400

    working on lambda function

--- lambda/lambda-handler/lambda_handler.go
+++ lambda/lambda-handler/lambda_handler.go
@@ -1,29 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"net/http"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"golang.org/x/net/context"
 )
 
-type APIGatewayProxyResponse struct {
-	StatusCode int               `json:"statusCode"`
-	Headers    map[string]string `json:"headers"`
-	Body       string            `json:"body"`
-}
+var tableName = "Pulumi-Resume-Table"
 
-func handler(req *http.Request) (APIGatewayProxyResponse, error) {
-	// Your logic here
-	responseBody := "{\"message\": \"Success!\"}"
+func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Get the value from the request body
+	valueToWrite := event.Body
 
-	response := APIGatewayProxyResponse{
-		StatusCode: http.StatusOK,
-		Headers: map[string]string{
-			"Content-Type": "application/json",
+	// Create a new DynamoDB session
+	sess := session.Must(session.NewSession())
+	svc := dynamodb.New(sess)
+
+	// Put the value into DynamoDB table
+	_, err := svc.PutItem(&dynamodb.PutItemInput{
+		Item: map[string]*dynamodb.AttributeValue{
+			"Value": {
+				S: aws.String(valueToWrite),
+			},
 		},
-		Body: responseBody,
+		TableName: aws.String(tableName),
+	})
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       fmt.Sprintf("Failed to write value to DynamoDB: %v", err),
+		}, nil
 	}
 
-	return response, nil
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Value written to DynamoDB table successfully.",
+	}, nil
 }
 
 func main() {
